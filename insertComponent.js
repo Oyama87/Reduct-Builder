237fa3d9be3e27a5bec09526610f025378829a27
Justin Taylor Goldberg <JustinTaylorG@Gmail.com>
2018-12-18 19:21:27 -0500

    Feature: Support relative paths as argument to create-component, including creating intermediate folders.

--- insertComponent.js
+++ insertComponent.js
@@ -8,27 +8,28 @@ const chalk = require('chalk')
 const { INDEX, METHODS, HOOKS, RENDER, LOCAL_STATE } = require('./constants')
 
 const lineArgs = process.argv.slice(2)
-const fileName = lineArgs[0] || 'SampleComponent'
+const pathName = lineArgs[0] || 'SampleComponent'
+const componentName = pathName.split('/').slice(-1)
 
-fs.mkdir(path.resolve(process.cwd(), fileName), (err) => {
+fs.mkdir(path.resolve(process.cwd(), pathName), {recursive: true}, (err) => {
   if(err) throw err
-  console.log(chalk.blueBright(`${fileName} directory created.`))
-  fs.writeFile(path.resolve(process.cwd(), fileName, 'index.js'), INDEX(fileName), (error) => {
+  console.log(chalk.blueBright(`${componentName} directory created.`))
+  fs.writeFile(path.resolve(process.cwd(), pathName, 'index.js'), INDEX(componentName), (error) => {
     if(error) throw error
   })
-  fs.writeFile(path.resolve(process.cwd(), fileName, 'methods.js'), METHODS, (error) => {
+  fs.writeFile(path.resolve(process.cwd(), pathName, 'methods.js'), METHODS, (error) => {
     if(error) throw error
   })
-  fs.writeFile(path.resolve(process.cwd(), fileName, 'hooks.js'), HOOKS, (error) => {
+  fs.writeFile(path.resolve(process.cwd(), pathName, 'hooks.js'), HOOKS, (error) => {
     if(error) throw error
   })
-  fs.writeFile(path.resolve(process.cwd(), fileName, 'render.js'), RENDER, (error) => {
+  fs.writeFile(path.resolve(process.cwd(), pathName, 'render.js'), RENDER, (error) => {
     if(error) throw error
   })
-  fs.writeFile(path.resolve(process.cwd(), fileName, 'localState.js'), LOCAL_STATE, (error) => {
+  fs.writeFile(path.resolve(process.cwd(), pathName, 'localState.js'), LOCAL_STATE, (error) => {
     if(error) throw error
   })
-  fs.writeFile(path.resolve(process.cwd(), fileName, 'styles.css'), `/* your style rules */`, (error) => {
+  fs.writeFile(path.resolve(process.cwd(), pathName, 'styles.css'), `/* your style rules */`, (error) => {
     if(error) throw error
   })
 })
